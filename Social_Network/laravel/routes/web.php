30ef0d837105f9f0302d08b18ea9cfc08c76f680
iamjoe <youssefashraf397@yahoo.com>
2019-04-18 03:02:00 +0200

    SocialNetwork

---
+++ Social_Network/laravel/routes/web.php
@@ -0,0 +1,80 @@
+<?php
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['middleware' => ['web']] , function(){
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::post('/signup', [
+    'uses' => 'UserController@postSignUp',
+    'as' => 'signup'
+]);
+Route::post('/signin', [
+    'uses' => 'UserController@postSignIn',
+    'as' => 'signin'
+]);
+
+Route::get('/dashboard', [
+    'uses' => 'SocialPostController@getDashboard',
+    'as' => 'dashboard',
+    'middleware' => 'auth'
+    
+]);
+
+Route::post('/createpost', [
+    'uses' => 'SocialPostController@postCreatePost',
+    'as' => 'post.create',
+    'middleware' => 'auth'
+   
+]);
+
+Route::get('/delete-post/{post_id}', [
+    'uses' => 'SocialPostController@getDeletePost',
+    'as' => 'post.delete',
+    'middleware' => 'auth'
+]);
+
+Route::get('/logout', [
+    'uses' => 'UserController@getLogout',
+    'as' => 'logout'
+]);
+
+Route::post('/edit', [
+    'uses' => 'SocialPostController@postEditPost',
+    'as' => 'edit'
+]);
+
+Route::get('/account', [
+    'uses' => 'UserController@getAccount',
+    'as' => 'account'
+]);
+
+Route::post('/upateaccount', [
+    'uses' => 'UserController@postSaveAccount',
+    'as' => 'account.save'
+]);
+
+Route::get('/userimage/{filename}', [
+    'uses' => 'UserController@getUserImage',
+    'as' => 'account.image'
+]);
+
+Route::post('/like', [
+    'uses' => 'SocialPostController@postLikePost',
+    'as' => 'like'
+]);
+});
